0400ad2bbe041d83a3432f24d2ef2a36de868194
lecepin <383810086@qq.com>
2022-02-09 00:55:59 +0800

    feat: tpl

---
+++ tpl/src/App.js
@@ -0,0 +1,7 @@
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+import "./styles.css";
+
+export default function App() {
+  return <div className="App"></div>;
+}
