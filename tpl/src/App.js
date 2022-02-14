efeedb0072d2d42c376cfe34d2e19bf44a10b841
lecepin <383810086@qq.com>
2022-02-14 13:18:55 +0800

    feat: tpl

--- tpl/src/App.js
+++ tpl/src/App.js
@@ -1,7 +1,29 @@
 import { createMachine } from "xstate";
 import { useMachine } from "@xstate/react";
+
 import "./styles.css";
 
+const toggleMachine = createMachine({
+  id: "toggle",
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      on: { TOGGLE: "inactive" },
+    },
+  },
+});
+
 export default function App() {
-  return <div className="App"></div>;
+  const [state, send] = useMachine(toggleMachine, { devTools: true });
+
+  return (
+    <button onClick={() => send("TOGGLE")}>
+      {state.value === "inactive"
+        ? "Click to activate"
+        : "Active! Click to deactivate"}
+    </button>
+  );
 }
--- tpl/src/index.js
+++ tpl/src/index.js
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import Split from "split.js";
+import { inspect } from "@xstate/inspect";
 
 import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById("root");
+inspect({
+  iframe: document.getElementById("xstate-inspect"),
+  url: "https://stately.ai/viz?inspect",
+});
+
+Split(["#xstate-inspect", "#root"], { minSize: 0 });
+
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  rootElement
+  document.getElementById("root")
 );
